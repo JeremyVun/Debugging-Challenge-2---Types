01f64c4a5a46b0e20c65fec6184bc23fb746fe25
Jeremy Vun <94739483+JeremyVun@users.noreply.github.com>
2021-11-20 14:39:40 +0000

    add hints

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import random
 
-a = random.randint(1,12)
-b = random.randint(1,12)
-
 for i in range(0, 10):
+  a = random.randint(1,12)
+  b = random.randint(1,12)
+  
   question = "What is " + a + " * " + b + "? "
+  
   answer = input(question)
-  print(type(answer))
 
   if answer == a * b:
     print ('Well done!')
